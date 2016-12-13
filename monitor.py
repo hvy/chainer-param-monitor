584c7d0e8aea29b99a7c9f6e42a7ee9cfd45adc6
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2016-12-13 06:51:04 +0000

    Naive percentile computation

--- monitor.py
+++ monitor.py
@@ -1,5 +1,6 @@
 import numpy as np
 from functools import reduce
+import chainer
 
 """
 A collection of functions that extract statistics for given models such as
@@ -9,14 +10,14 @@ instances of chainer.Chain in an dictionary.
 def get_params(model, param_names=('W', 'b')):
     """Return all parameters (weights and biases) for the given model."""
     params = _flattened_params(model, attr='data')
-    report = _as_report(params, prefix=model.name)
+    report = _as_report(params, prefix=model.name, xp=model.xp)
     return report
 
 
 def get_grads(model):
     """Return all currently stored gradient of the model."""
     grads = _flattened_params(model, attr='grad')
-    report = _as_report(grads, prefix=model.name + '/grad')
+    report = _as_report(grads, prefix=model.name + '/grad', xp=model.xp)
     return report
 
 
@@ -50,7 +51,7 @@ def _flattened_params(model, param_names=('W', 'b'), attr='data'):
     return params
 
 
-def _as_report(data, prefix=''):
+def _as_report(data, prefix='', xp=np):
     stats = {}
     for name in ['W', 'b']:
         d = data[name]
@@ -58,4 +59,14 @@ def _as_report(data, prefix=''):
         stats['{}/{}/max'.format(prefix, name)] = d.max()
         stats['{}/{}/mean'.format(prefix, name)] = d.mean()
         stats['{}/{}/std'.format(prefix, name)] = d.std()
+
+        if xp is chainer.cuda.cupy:
+            d = xp.asnumpy(d)
+
+        percentiles = np.percentile(d, (0.13, 2.28, 15.87, 50, 84.13, 97.72, 99.87))
+        percentiles = chainer.cuda.cupy.asarray(percentiles)
+
+        for i, p in enumerate(['n3s', 'n2s', 'n1s', 'z', '1s', '2s', '3s']):
+            stats['{}/{}/percentile/{}'.format(prefix, name, p)] = percentiles[i]
+
     return stats

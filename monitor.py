40e6af5a2e12a9ae128f4dc5708a822b7bbf41e9
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-01-23 11:00:34 +0000

    Minor arg changes

--- monitor.py
+++ monitor.py
@@ -4,95 +4,9 @@ import cupy
 import chainer
 
 
-# The name template of the statistic to collect and include in the report.
-# E.g. 'predictor/conv1/W/grad/percentile/sigma_one'
+# The name template of the statistic to collect and include in the report,
+# e.g. 'predictor/conv1/W/grad/percentile/sigma_one'
 key_template = '{model}/{layer}/{param}/{attr}/{statistic}'
-
-
-def _percentiles(data, sigma=(0.13, 2.28, 15.87, 50, 84.13, 97.72, 99.87)):
-
-    """Compute percentiles for data and return an array with the same length
-    as the number of elements in ``sigma``.
-
-    Args:
-        data (array): 1-dimensional NumPy or CuPy arryay.
-        sigma (tuple): Sigmas for which percentiles are computed. Defaults to
-            the three-sigma-rule. See: https://en.wikipedia.org/wiki/Percentile
-
-    Returns:
-        array: Array of percentiles.
-    """
-
-    # TODO: Make percentile computation faster for GPUs.
-
-    # To CPU before computing the percentiles since CuPy doesn't implement
-    # np.percentile().
-    if cupy.get_array_module(data) is cupy:
-        data = cupy.asnumpy(data)
-
-    try:
-        ps = np.percentile(data, sigma)
-    except IndexError:
-        # If data is missing from uninitialized parameters, add
-        # NaN placeholders instead of skipping the measurements completely
-        # or registering zeros.
-        ps = np.array((float('NaN'),) * 7)
-
-    # Back to GPU when percentiles are computed.
-    if cupy.get_array_module(data) is cupy:
-        ps = cupy.asarray(percentiles)
-
-    return ps
-
-
-def layer_params(layer, param_name, attr_name):
-
-    """Return parameters in a flattened array from the given layer or an empty
-    array if the parameters are not found.
-
-    Args:
-        layer (~chainer.Link): The layer from which parameters are collected.
-        param_name (str): Name of the parameter, ``'W'`` or ``'b'``.
-        attr_name (str): Name of the attribute, ``'data'`` or ``'grad'``.
-
-    Returns:
-        array: Flattened array of parameters.
-    """
-
-    if isinstance(layer, chainer.Chain):
-        # Nested chainer.Chain, aggregate all underlying statistics
-        return layers_params(layer, param_name, attr_name)
-    elif not hasattr(layer, param_name):
-        return layer.xp.array([])
-
-    params = getattr(layer, param_name)
-    params = getattr(params, attr_name)
-    return params.flatten()
-
-
-def layers_params(model, param_name, attr_name):
-
-    """Return all parameters in a flattened array from the given model.
-
-    Args:
-        model (~chainer.Chain): The model from which parameters are collected.
-        param_name (str): Name of the parameter, ``'W'`` or ``'b'``.
-        attr_name (str): Name of the attribute, ``'data'`` or ``'grad'``.
-
-    Returns:
-        array: Flattened array of parameters.
-    """
-
-    xp = model.xp
-    params = xp.array([], dtype=xp.float32)
-
-    for param in model.params():
-        if param.name == param_name:
-            values = getattr(param, attr_name)
-            values = values.flatten()
-            params = xp.concatenate((params, values))  # Slow?
-
-    return params
 
 
 def weight_statistics(model, layer_name=None):
@@ -229,8 +143,59 @@ def parameter_statistics(model, param_name, attr_name, layer_name=None):
     return as_statistics(lp, model.name, param_name, attr_name)
 
 
+def layer_params(layer, param_name, attr_name):
+
+    """Return parameters in a flattened array from the given layer or an empty
+    array if the parameters are not found.
+
+    Args:
+        layer (~chainer.Link): The layer from which parameters are collected.
+        param_name (str): Name of the parameter, ``'W'`` or ``'b'``.
+        attr_name (str): Name of the attribute, ``'data'`` or ``'grad'``.
+
+    Returns:
+        array: Flattened array of parameters.
+    """
+
+    if isinstance(layer, chainer.Chain):
+        # Nested chainer.Chain, aggregate all underlying statistics
+        return layers_params(layer, param_name, attr_name)
+    elif not hasattr(layer, param_name):
+        return layer.xp.array([])
+
+    params = getattr(layer, param_name)
+    params = getattr(params, attr_name)
+    return params.flatten()
+
+
+def layers_params(model, param_name, attr_name):
+
+    """Return all parameters in a flattened array from the given model.
+
+    Args:
+        model (~chainer.Chain): The model from which parameters are collected.
+        param_name (str): Name of the parameter, ``'W'`` or ``'b'``.
+        attr_name (str): Name of the attribute, ``'data'`` or ``'grad'``.
+
+    Returns:
+        array: Flattened array of parameters.
+    """
+
+    xp = model.xp
+    params = xp.array([], dtype=xp.float32)
+
+    for param in model.params():
+        if param.name == param_name:
+            values = getattr(param, attr_name)
+            values = values.flatten()
+            params = xp.concatenate((params, values))  # Slow?
+
+    return params
+
+
 def as_statistics(data, model_name, param_name, attr_name, *, layer_name=None,
-                  statistics=('min', 'max', 'mean', 'std', 'percentiles')):
+                  statistics=('min', 'max', 'mean', 'std'),
+                  percentiles=(0.13, 2.28, 15.87, 50, 84.13, 97.72, 99.87)):
 
     """Compute statistics based on the given data and return it as a ``dict``.
 
@@ -252,18 +217,15 @@ def as_statistics(data, model_name, param_name, attr_name, *, layer_name=None,
     if layer_name is None:
         layer_name = '*'
 
-    statistics = list(statistics)
-
-    if 'percentiles' in statistics:
-        statistics.pop(statistics.index('percentiles'))
-        percentiles = _percentiles(data)
-        for i, p in enumerate(['n3s', 'n2s', 'n1s', 'z', '1s', '2s', '3s']):
+    if percentiles:
+        ps = get_percentiles(data, sigma=percentiles)
+        for i, percentile in enumerate(ps):
             key = key_template.format(model=model_name,
                                       layer=layer_name,
                                       param=param_name,
                                       attr=attr_name,
-                                      statistic='percentile/{}'.format(p))
-            stats[key] = percentiles[i]
+                                      statistic='percentile/{}'.format(i))
+            stats[key] = percentile
 
     for s in statistics:
         key = key_template.format(model=model_name,
@@ -274,8 +236,36 @@ def as_statistics(data, model_name, param_name, attr_name, *, layer_name=None,
         try:
             stats[key] = getattr(data, s)()
         except ValueError:
-            # If data is invalid, e.g. for uninitialized linear links,
-            # chainer.links.Linear
+            # If data is missing from uninitialized model parameters, add
+            # NaN placeholders instead of skipping the measurements completely
+            # or registering zeros
             stats[key] = float('NaN')
 
     return stats
+
+
+def get_percentiles(data, sigma):
+
+    """Compute percentiles for data and return an array with the same length
+    as the number of elements in ``sigma``.
+
+    Args:
+        data (array): 1-dimensional NumPy or CuPy arryay.
+        sigma (tuple): Sigmas for which percentiles are computed.
+
+    Returns:
+        array: Array of percentiles.
+    """
+
+    def _get_percentiles(_data, _sigma):
+        try:
+            return np.percentile(_data, _sigma)
+        except IndexError:  # Handle uninitialized model parameters
+            return np.array((float('NaN'),) * 7)
+
+    if isinstance(data, cupy.ndarray):
+        # TODO(hvy): Make percentile computation faster for GPUs
+        data = cupy.asnumpy(data)
+        return cupy.asarray(_get_percentiles(data, sigma))
+
+    return _get_percentiles(data, sigma)

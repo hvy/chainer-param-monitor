2d6e5d0ec9671a30886666190c124b3d6b8f8fe2
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-01-24 07:18:02 +0000

    Slightly faster param concat

--- statistics.py
+++ statistics.py
@@ -9,18 +9,18 @@ key_template = '{link_name}/{param_name}/{attr_name}'
 
 def get_statistics(link, param_name, attr_name,
                    statistics=('min', 'max', 'mean', 'std'),
-                   percentile_sigmas=(0.13, 2.28, 15.87, 50, 84.13, 97.72, 99.87)):
+                   percentile_sigmas=(0.13, 2.28, 15.87, 50, 84.13, 97.72,
+                                      99.87)):
 
     key_base = key_template.format(link_name=link.name,
                                    param_name=param_name,
                                    attr_name=attr_name)
 
-    params = flattened_params(link, param_name, attr_name)
-
+    params = flatten_link(link, (param_name,), (attr_name,))
     stats = {}
 
     if percentile_sigmas:
-        percentiles = get_percentiles(params, sigma=percentile_sigmas)
+        percentiles = param_percentiles(params, sigma=percentile_sigmas)
         for i, percentile in enumerate(percentiles):
             stats['{}/percentile/{}'.format(key_base, i)] = percentile
 
@@ -37,60 +37,64 @@ def get_statistics(link, param_name, attr_name,
 
 
 def get_sparsity(link, include_bias=False):
-    xp = link.xp
 
-    def reduce_count_zeros(acc, param):
-        if param.name == 'W' or (include_bias and param.name == 'b'):
-            acc += param.data.size - xp.count_nonzero(param.data)
-        return acc
-
-    sparsity = reduce(reduce_count_zeros, link.params(), 0)
+    param_names = ('W', 'b') if include_bias else ('W',)
+    params = flatten_link(link, param_names, ('data',))
+    n_zeros = params.size - link.xp.count_nonzero(params)
 
     key = key_template.format(link_name=link.name,
                               param_name='Wb' if include_bias else 'W',
                               attr_name='zeros')
 
-    return { key: sparsity }
+    return { key: n_zeros }
 
 
-def flattened_params(link, param_name, attr_name):
+def flatten_link(link, param_names, attr_names):
 
-    # TODO(hvy): Support iterable param_names and attr_names.
-    xp = link.xp
-    params = xp.array([], dtype=xp.float32)
-
-    # TODO(hvy): Cleaner data collection without using concatenate inside loop
-    for param in link.params():
-        if param.name == param_name:
-            values = getattr(param, attr_name)
-            values = values.flatten()
-            params = xp.concatenate((params, values))
-
-    return params
-
-
-def get_percentiles(data, sigma):
-
-    """Compute percentiles for data and return an array with the same length
-    as the number of elements in ``sigma``.
+    """Flatten link parameters and return a 1-dimensional array located on the
+    same device as the link itself.
 
     Args:
-        data (array): 1-dimensional NumPy or CuPy arryay.
+        link (~chainer.Link): Link to flatten.
+        param_names (iterable): Parameter names to flatten,
+            e.g. ``('W', 'b')``.
+        attr_names (iterable): Attributes names to flatten,
+            e.g. ``('data', 'grad')``.
+    """
+
+    params = []
+    for param in link.params():
+        if param.name in param_names:
+            for attr_name in attr_names:
+                p = getattr(param, attr_name)
+                p = p.flatten()
+                params.append(p)
+
+    return link.xp.concatenate(params)
+
+
+def param_percentiles(params, sigma):
+
+    """Compute percentiles for given parameters and return an array with the
+    same length as the number of elements in ``sigma``.
+
+    Args:
+        params (array): 1-dimensional NumPy or CuPy arryay.
         sigma (tuple): Sigmas for which percentiles are computed.
 
     Returns:
         array: Array of percentiles.
     """
 
-    def _get_percentiles(_data, _sigma):
+    def _percentiles(_params, _sigma):
         try:
-            return np.percentile(_data, _sigma)
+            return np.percentile(_params, _sigma)
         except IndexError:  # Handle uninitialized model parameters
             return np.array((float('NaN'),) * 7)
 
-    if isinstance(data, cupy.ndarray):
-        # TODO(hvy): Make percentile computation faster for GPUs
-        data = cupy.asnumpy(data)
-        return cupy.asarray(_get_percentiles(data, sigma))
+    # TODO(hvy): Make percentile computation faster for GPUs
+    if isinstance(params, cupy.ndarray):
+        params = cupy.asnumpy(params)
+        return cupy.asarray(_percentiles(params, sigma))
 
-    return _get_percentiles(data, sigma)
+    return _percentiles(params, sigma)
